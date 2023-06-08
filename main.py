b3a0992357e8488cafddbd6932930b7f913f954c
AbhimanyuKapoor811 <135612554+AbhimanyuKapoor811@users.noreply.github.com>
2023-06-08 12:07:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+# Question 1: A string is a data type that stores words and is writien within double quotes, they can also be written within
+# single quotes but,
+# the single quotes help in printing the double quotes i a string if any present
+# A variable is a data type that is used for storing data they are by default in strings and can be changed by
+# specifying the data type that is required.
+
+#Question 3 : 23
+#Question 4: spamspamspam (ii) spamspamspam
+#Question 5 :  variables are usually in string format whereas 100 is an int hence 'eggs' is a valid variable name
+#Question 6: 99 is not a string so can't be concatenated
+#Question 7:
+# spam = int(input("Enter your value: "))
+# if(spam == 1):
+#     print("Hello")
+# elif(spam ==2):
+#     print("Howdy")
+# else:
+#     print("Greetings!")
+#Question 8: a break statement is used when we need to exit the loop for that iteration,
+#            A continue statement is used when we need to skip the iteration for that particular value
+#Question 9: range(10) will print numbers from 0-9
+#            range(0,10) will print 10 numbers starting from 0
+#            range(0,10,1) will print numbers starting from 0 incrementing 1 as we go
+# Question 10:
+# for loop:
+# list = [x for x in range(1,11)]
+# print(list)
+#Question 11: from spam import bacon
+#             spam.bacon()
+#Question 12: NoneType
+#Question 13: A try clause is used to suspect an error
+#             an except clause is used to run the code provided the error occurs in a try clause
+#Question 14:[] is basically an empty list
+# Question 15:
+# spam = [ x for x in range(2,12,2)]
+# print(spam)
+# spam[2] = 'hello'
+# print(spam)
+#Question 16:
+#   1.'d' '3'*2== 33, 33/11 == 3 spam[3] == 'd'
+#   2.'d'
+#   3.['a','b']
+# Question 17:
+#   1. 1
+#   2.[3.14,'cat', 11, 'cat', True, 99]
+#   3.[3.14, 11, 'cat', True, 99]
+#Question 18: append is used to add an element to the back of a list,
+#             Insert is used to push an element wherever in the list, the value comes before the index
+# Question 19: list.remove(), list.pop()
+#Question 20: A tuple is a list whose entries cannot be changed, it is represented in round braces, no modifictions can be made
+#             A list has data that can be changed , represented in []
+#Question 21:
+# my_list = ['0', '1']
+# tup = list(my_list)
+# print(tup)
+# tup = tuple(my_list)
+# print(tup)
+#Question 22: in copy() the existing varriable is changed throughout the code
+# in deep.copy() a new vr with the same name is created to solve teh iteration using that value
+#Question 23: empty_dict = {}
+#Question 24: a dictionary is a data type where values are present corresponding to keys
+#             in lists no such keys are present
+#Question 25: KeyError occurs as 'foo' is not a key in spam
+#Question 26:
+# spam = {'color': 99, 'main_course': 98}
+# if 'color' in spam:
+#     spam['color'] = 'black'
+#     print(spam)
